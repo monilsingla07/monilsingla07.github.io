a6acac69290dec2eeadada04497109706be67575
Monil Gupta <46887541+monilsingla07@users.noreply.github.com>
2026-01-28 14:52:10 +0530

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-// If your GitHub Pages site is served from the root domain (ahamstree.com),
-// base should be '/'. If using username.github.io/repo, set base: '/repo/'.
-export default defineConfig({
-  base: "/"
-});
-
