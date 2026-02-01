7c11099cf9c44c5dfdd567958ff856ca3d690215
Monil Gupta <46887541+monilsingla07@users.noreply.github.com>
2026-02-01 10:46:38 +0530

    Update supabase.js

--- assets/supabase.js
+++ assets/supabase.js
@@ -4,5 +4,11 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 export const SUPABASE_URL = "https://mgmgkwoxirvzdnmayhwq.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_mS_exnQ_Am8_GWMNgCb63w_PfvFA7Mz";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
 
