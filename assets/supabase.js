7e0dda06f7288af0a2fdf4c1c5bb11e30dc7b1ba
Monil Gupta <46887541+monilsingla07@users.noreply.github.com>
2026-02-01 11:11:31 +0530

    Update supabase.js

--- assets/supabase.js
+++ assets/supabase.js
@@ -1,8 +1,9 @@
 // assets/supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = "https://mgmgkwoxirvzdnmayhwq.supabase.co"; // ✅ correct  
+export const SUPABASE_URL = "https://mgmgkwoxirvzdnmayhwq.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_mS_exnQ_Am8_GWMNgCb63w_PfvFA7Mz";
+
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
